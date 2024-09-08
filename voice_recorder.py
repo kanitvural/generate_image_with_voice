886c276daee55bbe67510cd26ab1f874e9b1133d
kanitvural <kanitvural@gmail.com>
2024-09-08 11:47:50 +0300

    voice_recorder complated

--- voice_recorder.py
+++ voice_recorder.py
@@ -1,4 +1,31 @@
-import pyaudio
 import wave
 
+import pyaudio
 
+
+def record(record_active, frames):
+    audio = pyaudio.PyAudio()
+
+    stream = audio.open(
+        format=pyaudio.paInt16,
+        channels=1,
+        rate=44100,
+        input=True,
+        frames_per_buffer=1024,
+    )
+
+    while record_active.is_set():  # Multithreading
+        data = stream.read(num_frames=1024, exception_on_overflow=False)
+        frames.append(data)
+        
+    stream.stop_stream()
+    stream.close()
+    audio.terminate()
+    
+    # save audiofile with same format
+    audio_file = wave.open("voice_prompt.wav", "wb")
+    audio_file.setnchannels(1)
+    audio_file.setsampwidth(audio.get_sample_size(pyaudio.paInt16))
+    audio_file.setframerate(44100)
+    audio_file.writeframes(b''.join(frames))
+    audio_file.close()
